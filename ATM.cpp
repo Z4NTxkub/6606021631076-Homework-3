434cfb3f7315026c853a359590bf060b2a995dbf
Z4NTxkub <139094273+Z4NTxkub@users.noreply.github.com>
2023-07-23 17:35:19 +0700

    Add files via upload

---
+++ ATM.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int m ;
+	cout << "Amount to be withdraw : ";
+	cin >> m;
+	cout << "1000 : " << m/1000					<< endl;
+	cout << "100 : " << (m%1000)/100			<< endl;
+	cout << "50 : " << ((m%1000)%100)/50		<< endl;
+	cout << "20 : " << (((m%1000)%100)%50)/20	<< endl;
+
+	system("pause");
+	return(0);
+}
